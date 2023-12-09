7939c9fa65a218afaca0ab90902ed58671c490ba
hongquang1412 <hongquangst456@gmail.com>
2023-12-09 14:41:28 +0700

    commit folder test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { TestComponent } from './test/test.component';
 
-export const routes: Routes = [];
+// config url 
+export const routes: Routes = [{ path: 'test', component: TestComponent }];
---
+++ src/app/test/test.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-test',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.scss',
+})
+export class TestComponent {
+  isTabActive= true;
+  user = {
+    type: 'text',
+    name: 'Hong Quang',
+    age: 23,
+  };
+
+  show(mes: string) {
+    alert(`mess: ${mes}`);
+  }
+
+  authors = [
+    {
+      id: 1,
+      firstName: "Flora",
+      lastName: "Twell",
+      email: "ftwell0@phoca.cz",
+      gender: "Female",
+      ipAddress: "99.180.237.33",
+    },
+    {
+      id: 2,
+      firstName: "Priscella",
+      lastName: "Signe",
+      email: "psigne1@berkeley.edu",
+      gender: "Female",
+      ipAddress: "183.243.228.65",
+    },
+
+  ];
+}
